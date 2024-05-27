fcf47326fa97d75d172b986e9fb8b782bb127090
Berkay Geyik <berkay.geyik@tum.de>
2024-05-27 18:40:25 +0200

    small changes

--- SeqTrack/lib/train/admin/local.py
+++ SeqTrack/lib/train/admin/local.py
@@ -3,8 +3,8 @@ class EnvironmentSettings:
         self.workspace_dir = 'C:\\Users\\berka\\OneDrive\\VideoX\\SeqTrack'    # Base directory for saving network checkpoints.
         self.tensorboard_dir = 'C:\\Users\\berka\\OneDrive\\VideoX\\SeqTrack\\tensorboard'    # Directory for tensorboard files.
         self.pretrained_networks = 'C:\\Users\\berka\\OneDrive\\VideoX\\SeqTrack\\pretrained_networks'
-        self.carotidartery_dir = 'C:\\Users\\berka\\OneDrive\\VideoX\\SeqTrack\\data\\carotidartery'
         self.lasot_dir = 'D:\\data\\lasot'
+        self.carotidartery_dir = 'C:\\Users\\berka\\OneDrive\\VideoX\\SeqTrack\\data\\carotidartery'
         self.got10k_dir = 'C:\\Users\\berka\\OneDrive\\VideoX\\SeqTrack\\data\\got10k'
         self.lasot_lmdb_dir = 'C:\\Users\\berka\\OneDrive\\VideoX\\SeqTrack\\data\\lasot_lmdb'
         self.got10k_lmdb_dir = 'C:\\Users\\berka\\OneDrive\\VideoX\\SeqTrack\\data\\got10k_lmdb'
